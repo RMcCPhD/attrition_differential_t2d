97620f305c5844453472f8c6fa498ed0847e14fa
rmcchrystal <r.mcchrystal.1@research.gla.ac.uk>
2025-06-13 16:31:40 +0100

    Importing regression function

---
+++ scripts/99_summarise.R
@@ -0,0 +1,12 @@
+
+# Change file number when additional scripts added for obj2 etc
+source("scripts/00_config.R")
+source("scripts/00_packages.R")
+
+# Import prepared aggregate data
+a_imp_df <- readRDS("processed_data/tidy_agg_n391.rds") %>% 
+  mutate(
+    atc = factor(atc, levels = c("placebo", setdiff(unique(atc), "placebo"))),
+    class_short = factor(class_short),
+    class_short = relevel(class_short, ref = "placebo")
+  )
