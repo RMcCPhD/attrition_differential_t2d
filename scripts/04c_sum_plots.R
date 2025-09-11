aba6aaa6838c3bb074d6f53e48c4662ba40dadfd
Ryan <r.mcchrystal.1@research.gla.ac.uk>
2025-09-11 17:58:19 +0100

    Fig1 update

--- scripts/04c_sum_plots.R
+++ scripts/04c_sum_plots.R
@@ -27,7 +27,7 @@ a_imp_agg %>%
   inner_join(a_imp_agg) %>% 
   write_csv("scratch_data/manual_assign_exp.csv")
 
-# Join to rest
+# Join to rest, add comparator id
 b_imp_fix <- read_csv("scratch_data/manual_assigned_exp.csv")
 
 b_agg <- a_imp_agg %>% 
@@ -39,38 +39,64 @@ b_agg <- a_imp_agg %>%
       class %in% c("dpp4", "glp1", "sglt2") ~ class,
       class == "placebo" ~ class,
       TRUE ~ "classic"
+    ),
+    attr_prop = round(attr_rate * 100, 2),
+    comp = case_when(
+      exp != trttype ~ trttype,
+      TRUE ~ NA
     )
-  )
-
-# Plot
-plot_rates <- b_agg %>% 
+  ) %>% 
+  group_by(trial_id) %>% 
+  fill(comp, .direction = "downup") %>% 
   mutate(
+    comp = case_when(
+      length(unique(comp)) == 2 & "placebo" %in% comp ~ "placebo",
+      TRUE ~ comp
+    )
+  ) %>% 
+  ungroup()
+
+# Arrange by total attrition per trial, compute total attrition
+# Setup factors for plot
+b_agg_index <- b_agg %>% 
+  group_by(trial_id) %>% 
+  mutate(attr_total = round(sum(arm_attr)/sum(arm_n) * 100, 2)) %>% 
+  ungroup() %>% 
+  arrange(attr_total) %>% 
+  mutate(
+    order_id = match(trial_id, unique(trial_id)),
     trttype = case_match(
       trttype,
-      "classic" ~ "Older ADs",
+      "classic" ~ "Other ADs",
       "placebo" ~ "Placebo",
       "dpp4" ~ "DPP4",
       "glp1" ~ "GLP-1",
       "sglt2" ~ "SGLT2",
       .default = trttype
-    ),,
+    ),
     trttype = factor(
       trttype,
-      levels = c("DPP4", "GLP-1", "SGLT2", "Older ADs", "Placebo")
+      levels = c("DPP4", "GLP-1", "SGLT2", "Other ADs", "Placebo")
     ),
     exp = case_match(
       exp,
       "dpp4" ~ "DPP4",
       "glp1" ~ "GLP-1",
       "sglt2" ~ "SGLT2"
-    )
+    ),
+    comp = fct_relevel(factor(comp), "placebo", "classic", "dpp4")
   ) %>% 
-  ggplot(aes(x = attr_rate, y = trial_id, colour = trttype)) +
+  arrange(comp, order_id, trial_id) %>% 
+  mutate(trial_id = factor(trial_id, levels = unique(trial_id)))
+
+# Plot
+plot_rates <- b_agg_index %>% 
+  ggplot(aes(x = attr_prop, y = fct_rev(trial_id), colour = trttype)) +
   geom_line(aes(group = trial_id), linewidth = 0.4, colour = "black") +
   geom_point() +
   scale_color_manual(
     values = c(
-      "Older ADs" = "forestgreen",
+      "Other ADs" = "forestgreen",
       "DPP4" = "steelblue",
       "GLP-1" = "gold1",
       "SGLT2" = "darkorange1",
@@ -78,8 +104,14 @@ plot_rates <- b_agg %>%
     )
   ) +
   facet_wrap(~exp, scales = "free") +
-  labs(x = "Attrition proportion", y = NULL, colour = "Treatment type") +
-  theme_classic()
+  labs(x = "Total attrition (%)", y = NULL, colour = "Treatment type") +
+  scale_x_continuous(limits = c(0, 60), n.breaks = 6) +
+  theme_classic() +
+  theme(
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank()
+    # axis.text.y = element_text(size = 20, colour = "white")
+  )
 
 plot_rates
 
